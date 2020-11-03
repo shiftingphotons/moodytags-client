280bce9ed0f92d312a8dab377428f27680e85454
Dimitar Ralev <dralev@pm.me>
2020-11-03 11:53:01 +0200

    Improve playlist tagging

--- src/components/TagMenu.vue
+++ src/components/TagMenu.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <v-menu
+    <v-dialog
       v-model="menu"
+      width="420"
       :close-on-content-click="false"
-      max-width="360"
-      offset-x
     >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -18,44 +17,50 @@
           <v-icon small>fal fa-tags</v-icon>
         </v-btn>
       </template>
-      <v-card>
-        <v-card-title>Selected</v-card-title>
-        <v-chip-group>
-          <v-chip
-            v-for="(tag, index) in playlist.tags"
-            :key="index"
-            class="ma-2"
-            @click:close="removeTag(tag, index)"
-            close
+        <v-card>
+          <v-autocomplete
+            class="pa-4"
+            v-model="playlist.tags"
+            :items="tags"
+            append-icon="fal fa-caret-down"
+            item-text="name"
+            item-value="name"
+            color="blue-grey lighten-2"
+            chips
+            multiple
+            :search-input.sync="searchInput"
+            @change="searchInput = null"
           >
-            {{ tag }}
-          </v-chip>
-        </v-chip-group>
-        <v-divider></v-divider>
-
-        <v-chip-group
-          v-for="(collection, inde) in tags"
-          :key="inde"
-          multiple
-          column
-          >
-          <v-card-title>{{ collection.name }}</v-card-title>
-          <v-chip
-            v-for="(tag, ind) in collection.tags"
-            :key="tag"
-            :class="playlist.tags.indexOf(tag) != -1 ? 'ma-2 selected' : 'ma-2'"
-            @click="addTag(tag, ind)"
-          >
-            {{ tag }}
-          </v-chip>
-        </v-chip-group>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" @click="tagPlaylist(playlist.id)" text>Save</v-btn>
-          <v-btn color="primary" @click="menu = false" text>Close</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-menu>
+            <template v-slot:selection="data">
+              <v-chip
+                v-bind="data.attrs"
+                :input-value="data.selected"
+                close
+                close-icon="fal fa-times-circle"
+                @click="data.select"
+                @click:close="removeTag(data.item.name)"
+              >
+                {{ data.item.name }}
+              </v-chip>
+            </template>
+            <template v-slot:item="data">
+              <template>
+                <v-list-item-icon>
+                  <v-icon size="medium">fal fa-hashtag</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-html="data.item.name"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+            </template>
+          </v-autocomplete>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="tagPlaylist(playlist.id)" text>Save</v-btn>
+            <v-btn color="primary" @click="closeDialog" text>Close</v-btn>
+          </v-card-actions>
+        </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -64,12 +69,13 @@ const axios = require('axios');
 
 export default {
   name: "TagMenu",
-  data: () => ({
-    fav: true,
-    menu: false,
-    message: false,
-    hints: true,
-  }),
+  data: function() {
+    return {
+      menu: false,
+      originalTags: [...this.playlist.tags],
+      searchInput: null
+    }
+  },
   props: ['playlist', 'tags'],
   methods: {
     addTag: function(tag) {
@@ -88,6 +94,10 @@ export default {
         index = this.playlist.tags.indexOf(tag)
       }
       this.playlist.tags.splice(index, 1)
+    },
+    closeDialog: function() {
+      this.playlist.tags = [...this.originalTags]
+      this.menu = false;
     },
     tagPlaylist: function(ext_id) {
       var url, method, data;
@@ -117,4 +127,9 @@ export default {
 
 <style scoped>
 .selected { background: #00AA95 !important; color: white !important; }
+</style>
+<style>
+.v-select-list > .v-subheader {
+  font-weight: 800;
+}
 </style>
